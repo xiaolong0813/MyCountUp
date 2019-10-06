cd78cde5dbcdea8214cac3c47064d6f648314bb9
xuxiaolong <xuxiaolong200691@163.com>
2019-10-06 19:54:40 +0800

    packaging finished

---
+++ src/CountUp.vue
@@ -0,0 +1,57 @@
+<template>
+    <span ref="countup"></span>
+</template>
+
+<script>
+    import {CountUp} from 'countup.js'
+    export default {
+        name: 'countup-demo',
+        data () {
+            return {
+                countup: null
+            }
+        },
+        props: {
+            end: {
+                type: Number,
+                default: 2019
+            },
+            start: {
+                type: Number,
+                default: 0
+            },
+            decimal: {
+                type: Number,
+                default: 0
+            },
+            duration: {
+                type: Number,
+                default: 2.5
+            },
+            prefix: {
+                type: String,
+                default: ''
+            }
+        },
+        mounted() {
+            this.initCountup()
+        },
+        methods: {
+            initCountup() {
+                this.countup = new CountUp(this.$refs.countup, this.end ,{
+                    startVal: this.start,
+                    duration: this.duration,
+                    prefix: this.prefix,
+                    decimalPlaces: this.decimal
+                })
+                this.countup.start()
+            }
+        }
+
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
